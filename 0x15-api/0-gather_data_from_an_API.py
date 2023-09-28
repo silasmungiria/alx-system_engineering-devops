77ea0ad8a67ad6e389a08f4b1a77b3bbb4bc06ec
mugambi12 <silasmungiria16@gmail.com>
2023-09-29 02:27:22 +0300

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,10 +7,13 @@ returns information about his/her TODO list progress.
 import requests
 from sys import argv
 
+
+id_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}/todos'
+name_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
+
+
 def get_employee_data(employee_id):
     try:
-        id_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}/todos'
-        name_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
 
         response = requests.get(id_url)
         response.raise_for_status()
@@ -24,13 +27,14 @@ def get_employee_data(employee_id):
         return employee_data, name
 
     except requests.exceptions.HTTPError as errh:
-        print ("HTTP Error:",errh)
+        print("HTTP Error:", errh)
     except requests.exceptions.ConnectionError as errc:
-        print ("Error Connecting:",errc)
+        print("Error Connecting:", errc)
     except requests.exceptions.Timeout as errt:
-        print ("Timeout Error:",errt)
+        print("Timeout Error:", errt)
     except requests.exceptions.RequestException as err:
-        print ("Something went wrong:",err)
+        print("Something went wrong:", err)
+
 
 if __name__ == "__main__":
     if len(argv) != 2:
@@ -44,9 +48,10 @@ if __name__ == "__main__":
     if employee_data is not None and name is not None:
         total_tasks = len(employee_data)
         completed_tasks = sum(task['completed'] for task in employee_data)
-        
-        print(f"Employee {name} is done with tasks({completed_tasks}/{total_tasks}):")
-        
+
+        print(f"Employee {name} is done with tasks"
+              f"({completed_tasks}/{total_tasks}):")
+
         for task in employee_data:
             if task['completed']:
                 print(f"\t{task['title']}")

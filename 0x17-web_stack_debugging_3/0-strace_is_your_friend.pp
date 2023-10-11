1e49fa3822b41c0dff20f3830a9384e13a64bfe2
mugambi12 <silasmungiria16@gmail.com>
2023-10-12 02:00:08 +0300

    Strace is your friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,26 +1,6 @@
-# Puppet manifest for fixing Apache 500 error
+# Puppet manifest to fix a bug in wp-setings.php
 
-# Define an exec resource to run strace on the Apache process
-exec { 'strace_apache':
-  command => 'strace -o /tmp/strace_output -p $(pgrep apache2)',
-}
-
-# Define an exec resource to fix the identified issue
-exec { 'fix_apache_error':
-  command     => '/path/to/fix_script.sh',
-  refreshonly => true,
-  subscribe   => Exec['strace_apache'],
-}
-
-# Define a file resource to ensure the fix script is present
-file { '/path/to/fix_script.sh':
-  ensure => file,
-  source => 'puppet:///modules/module_name/fix_script.sh',
-  mode   => '0755',
-}
-
-# Notify a service resource to restart Apache after the fix
-service { 'apache2':
-  ensure    => running,
-  subscribe => Exec['fix_apache_error'],
+exec { 'fix the php extension issue':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
